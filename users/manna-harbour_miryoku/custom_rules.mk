d349a87918d993b4bb2a6d750de71464a923fa0b
Lynn Ly <lynnly1200@gmail.com>
2022-08-05 00:15:23 -0700

    Enable RGB matrix and RBGLIGHT

--- users/manna-harbour_miryoku/custom_rules.mk
+++ users/manna-harbour_miryoku/custom_rules.mk
@@ -2,5 +2,9 @@
 # https://github.com/manna-harbour/miryoku
 
 CAPS_WORD_ENABLE = yes
-BACKLIGHT_ENABLE = no
+# BACKLIGHT_ENABLE = yes
+RGBLIGHT = yes # RGB Underglow
 VIA_ENABLE = no
+
+RGB_MATRIX_ENABLE = yes # Per-key leds
+RGB_MATRIX_DRIVER = WS2812
