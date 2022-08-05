1c757531a7343aa305b8af774a2c02010935b48a
Lynn Ly <lynnly1200@gmail.com>
2022-08-05 08:07:15 -0700

    Remove RBG_LIGHT and use only RGB_MATRIX

--- users/manna-harbour_miryoku/custom_rules.mk
+++ users/manna-harbour_miryoku/custom_rules.mk
@@ -2,9 +2,9 @@
 # https://github.com/manna-harbour/miryoku
 
 CAPS_WORD_ENABLE = yes
-# BACKLIGHT_ENABLE = yes
-RGBLIGHT = yes # RGB Underglow
+BACKLIGHT_ENABLE = no # For single color backlighting
+RGBLIGHT = no # RGB Underglow ONLY - not compatible with matrix
 VIA_ENABLE = no
 
-RGB_MATRIX_ENABLE = yes # Per-key leds
+RGB_MATRIX_ENABLE = yes # Per-key leds and underglow
 RGB_MATRIX_DRIVER = WS2812
